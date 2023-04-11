aa718639922065da46c60178ac194bdecb66f27d
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2023-04-10 23:56:31 -0300

    Exemplos práticos e úteis

--- main.js
+++ main.js
@@ -1,24 +1,98 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/**
+ * Nullish Coalescing Operator
+ */
+// const idade = null;
+// const idade = 0;
+// const idade = 27;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// ?? ignora apenas o null, || ignora null e 0
+// document.getElementById('nullish-coalescing').innerText = 'Sua idade é: ' + (idade ?? 'Não informado');
 
-setupCounter(document.querySelector('#counter'))
+
+/**
+ * Objetos
+ */
+// const usuario = {
+//   nome: 'Gustavo',
+//   nickname: 'Ratt',
+//   idade: 27,
+//   endereco: {
+//     rua: 'Rua teste',
+//     numero: 176
+//   }
+// };
+
+// Expressão in
+// document.getElementById('objeto').innerText = ('nome' in usuario);
+// document.getElementById('objeto').innerText = ('nickname' in usuario);
+
+// Retornar chaves
+// document.getElementById('objeto').innerText = Object.keys(usuario);
+
+// Retornar valores
+// document.getElementById('objeto').innerText = JSON.stringify(Object.values(usuario));
+
+// Retorna vetor com diversos vetores e cada um deles contendo uma chave e valor
+// document.getElementById('objeto').innerText = JSON.stringify(Object.entries(usuario));
+
+// Desestruturação
+// const { endereco, idade: idade_usuario, nickname = 'Gu' } = usuario; // Renomeando variável idade para idade_usuario
+
+// document.getElementById('objeto').innerText = JSON.stringify({ endereco, idade_usuario, nickname });
+
+
+/**
+ * Rest operator
+ */
+// Separa o nome do resto das chaves
+// const { nome, ...rest } = usuario;
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const [primeiro, segundo, ...resto] = array;
+// const [primeiro, , terceiro, ...resto] = array; // Se deixar apenas a virgula, pula o índice
+
+// document.getElementById('rest-operator').innerText = JSON.stringify({primeiro, segundo, resto});
+// document.getElementById('rest-operator').innerText = JSON.stringify({primeiro, terceiro, resto});
+
+
+/**
+ * Short Syntax
+ */
+// const nome = 'Gustavo';
+// const idade = 27;
+
+// const usuario = {
+//   nome,
+//   idade
+// };
+
+// document.getElementById('short-syntax').innerText = JSON.stringify(usuario);
+
+
+/**
+ * Optional Chaining
+ */
+const usuario = {
+  nome: 'Gustavo',
+  nickname: 'Ratt',
+  idade: 27,
+  endereco: {
+    rua: 'Rua teste',
+    numero: 176,
+    cep: {
+      codigo: '80530000',
+      cidade: 'Curitiba'
+    },
+    // exibirEnderecoCompleto() {
+    //   return 'ok';
+    // }
+  }
+};
+
+// Interrogação em cada propriedade que pode não existir e
+// faz um nullish coalescing caso não exista alguma propriedade para exibir um valor padrão
+// document.getElementById('optional-chaining').innerText = usuario?.endereco?.cep?.codigo ?? 'Não informado';
+
+// Optional Chaining em função
+document.getElementById('optional-chaining').innerText = usuario?.endereco?.exibirEnderecoCompleto?.() ?? 'Não existe função';
